146681f470f8205231f8387ae39ad75515592e03
matthieugomez <gomez.matthieu@gmail.com>
2020-08-20 18:48:24 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,13 @@ df = DataFrame(x = [1, 2], y = [3, 4], z = [5, 6])
 @test combine(df, @cols(z1 = sum(x), z2 = sum(y))).z2 == [sum(df.y)]
 @test transform(df, @rows(z = x + y)).z == df.x .+ df.y
 @test combine(df, @cols(z1 = sum(x), z2 = sum(y))).z2 == [sum(df.y)]
-@test size(filter(@cols(x > 1), df), 2) == 1
+@test size(filter(@cols(x > 1), df), 1) == 1
 
 # test $
 u = :y
-@test transform(df, @cols(z = sum($u))).z == sum(df.u)
+@test combine(df, @cols(z = sum($u))).z == [sum(df.y)]
 @test transform(df, @rows($u = x)).y == df.x
 
 # test obj
 global u = [3, 4]
-transform(df, @cols(z = sum(^(u)))).z == sum(df.u)
+transform(df, @cols(z = sum(^(u)))).z == sum(u)
